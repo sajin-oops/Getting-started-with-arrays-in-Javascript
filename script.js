29bb2528405d3518d48ae0cc5c71e74b8b2173b2
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-03 18:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let cities = ["Chennai","Madurai","Coimbatore","Kanyakumari"]
+let marks = [99,99,98,96,97]
+// length = total elements in the array
+console.log(cities.length)
+//4
+console.log(marks.length)
+//5
+console.log(cities[1])
+//Madurai
+console.log(marks[4])
+//97
+console.log(marks[marks.length-1])
+                //97 last element 
+// accessing last element in an array
+
+
+
